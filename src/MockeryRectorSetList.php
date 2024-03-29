3e99b505426e107e4e5d971e98c54ebf34c6f3d5
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-03-29 13:17:37 -0500

    Update MockeryRectorSetList.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/MockeryRectorSetList.php
+++ src/MockeryRectorSetList.php
@@ -8,9 +8,9 @@ use Rector\Set\Contract\SetListInterface;
 
 final readonly class MockeryRectorSetList implements SetListInterface
 {
-    final public const MOCKERY_16 = __DIR__ . '/../config/mockery-16.php';
+    final public const MOCKERY_1_6 = __DIR__ . '/../config/mockery-1.6.php';
 
-    final public const MOCKERY_20 = __DIR__ . '/../config/mockery-20.php';
+    final public const MOCKERY_2_0 = __DIR__ . '/../config/mockery-2.0.php';
 
     final public const PHPUNIT_TO_MOCKERY = __DIR__ . '/../config/phpunit-to-mockery.php';
 
