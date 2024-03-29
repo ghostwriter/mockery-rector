e642b5b22161f67c73f508c5230f21f6b56f7132
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-03-29 14:34:25 -0500

    Create ShouldReceiveToAllowsRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Rule/ShouldReceiveToAllowsRectorTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\MockeryRectorTests\Unit\Rule;
+
+use Ghostwriter\MockeryRector\AbstractMockeryRector;
+use Ghostwriter\MockeryRector\Rule\ShouldReceiveToAllowsRector;
+use Ghostwriter\MockeryRectorTests\Unit\AbstractMockeryRectorTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(ShouldReceiveToAllowsRector::class)]
+#[CoversClass(AbstractMockeryRector::class)]
+final class ShouldReceiveToAllowsRectorTest extends AbstractMockeryRectorTestCase
+{
+}
