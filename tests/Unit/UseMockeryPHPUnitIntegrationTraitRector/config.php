09c066603907240ccdf387319635a183cc58c472
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-03-29 14:31:49 -0500

    Delete config.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/UseMockeryPHPUnitIntegrationTraitRector/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Ghostwriter\MockeryRector\Rule\UseMockeryPHPUnitIntegrationTraitRector;
-use Rector\Config\RectorConfig;
-
-return RectorConfig::configure()->withRules([UseMockeryPHPUnitIntegrationTraitRector::class]);
