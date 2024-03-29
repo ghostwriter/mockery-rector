7e178823323917f5bc74b4793e81c26c32bb4b55
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-03-29 13:16:35 -0500

    Delete up-to-mockery-16.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- config/up-to-mockery-16.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Ghostwriter\MockeryRector\MockeryRectorSetList;
-use Rector\Config\RectorConfig;
-
-return RectorConfig::configure()->withSets([MockeryRectorSetList::MOCKERY_16]);
