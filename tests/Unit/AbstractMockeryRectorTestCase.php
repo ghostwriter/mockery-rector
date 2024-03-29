d3d9317a80b53ece79c1f31b2bae77e1a92017c3
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-03-29 14:34:11 -0500

    Update AbstractMockeryRectorTestCase.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/AbstractMockeryRectorTestCase.php
+++ tests/Unit/AbstractMockeryRectorTestCase.php
@@ -6,18 +6,26 @@ namespace Ghostwriter\MockeryRectorTests\Unit;
 
 use Generator;
 use Ghostwriter\MockeryRector\AbstractMockeryRector;
+use InvalidArgumentException;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\UsesClass;
 use Rector\Testing\PHPUnit\AbstractRectorTestCase;
 use ReflectionClass;
+use function array_key_exists;
 use function dirname;
+use function realpath;
+use function sprintf;
+use function strrpos;
+use function substr;
 
 #[UsesClass(AbstractMockeryRector::class)]
 abstract class AbstractMockeryRectorTestCase extends AbstractRectorTestCase
 {
+    protected static $filePaths = [];
+
     final public function provideConfigFilePath(): string
     {
-        return self::currentDirectory('/config.php');
+        return self::fixtureDirectory('/config.php');
     }
 
     #[DataProvider('provideData')]
@@ -27,15 +35,47 @@ abstract class AbstractMockeryRectorTestCase extends AbstractRectorTestCase
     }
 
     /**
+     * @return string
+     */
+    public static function currentRuleName(): string
+    {
+        return substr(static::class, 1 + (strrpos(static::class, '\\') ?: -1), -4);
+    }
+
+    /**
      * @return Generator
      */
     final public static function provideData(): Generator
     {
-        yield from self::yieldFilesFromDirectory(self::currentDirectory('/Fixture'));
+        yield from self::yieldFilesFromDirectory(self::fixtureDirectory());
     }
 
-    private static function currentDirectory(string $path): string
+    private static function fixtureDirectory(string $path = '/'): string
     {
-        return dirname((new ReflectionClass(static::class))->getFileName()) . $path;
+        if (! array_key_exists(static::class, self::$filePaths)) {
+            $filePath = dirname((new ReflectionClass(static::class))->getFileName());
+
+            $ruleName = self::currentRuleName();
+
+            $rulePath = '/tests/Fixture/' . $ruleName . '/';
+
+            while (realpath($filePath . $rulePath) === false) {
+                $filePath = dirname($filePath);
+
+                if ($filePath === '/') {
+                    throw new InvalidArgumentException(
+                        sprintf(
+                            'Fixture path for "%s" does not exist in %s or any parent directory',
+                            $ruleName,
+                            $rulePath
+                        )
+                    );
+                }
+            }
+
+            self::$filePaths[static::class] = $filePath . $rulePath;
+        }
+
+        return self::$filePaths[static::class] . $path;
     }
 }
