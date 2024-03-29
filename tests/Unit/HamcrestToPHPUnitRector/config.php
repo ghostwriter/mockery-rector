4403422139110ab5639440e5d0e9c5e85154fe1f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-03-29 14:32:28 -0500

    Delete config.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/HamcrestToPHPUnitRector/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Ghostwriter\MockeryRector\Rule\HamcrestToPHPUnitRector;
-use Rector\Config\RectorConfig;
-
-return RectorConfig::configure()->withRules([HamcrestToPHPUnitRector::class]);
