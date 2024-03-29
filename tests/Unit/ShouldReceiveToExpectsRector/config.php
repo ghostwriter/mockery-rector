ae358f4a806f390c3e30292d4b6a2ac0d9c79401
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-03-29 14:31:55 -0500

    Delete config.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/ShouldReceiveToExpectsRector/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Ghostwriter\MockeryRector\Rule\ShouldReceiveToExpectsRector;
-use Rector\Config\RectorConfig;
-
-return RectorConfig::configure()->withRules([ShouldReceiveToExpectsRector::class]);
