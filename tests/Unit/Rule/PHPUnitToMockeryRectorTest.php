9877cc74bb5311a7361dd3dc087546bffe18da2f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-23 18:47:34 -0500

    Update PHPUnitToMockeryRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Rule/PHPUnitToMockeryRectorTest.php
+++ tests/Unit/Rule/PHPUnitToMockeryRectorTest.php
@@ -2,11 +2,11 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\MockeryRectorTests\Unit\Rule;
+namespace Tests\Unit\Rule;
 
 use Ghostwriter\MockeryRector\AbstractMockeryRector;
 use Ghostwriter\MockeryRector\Rule\PHPUnitToMockeryRector;
-use Ghostwriter\MockeryRectorTests\Unit\AbstractMockeryRectorTestCase;
+use Tests\Unit\AbstractMockeryRectorTestCase;
 use PHPUnit\Framework\Attributes\CoversClass;
 
 #[CoversClass(PHPUnitToMockeryRector::class)]
