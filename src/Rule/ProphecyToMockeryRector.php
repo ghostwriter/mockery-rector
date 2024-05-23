fda447e3304ce6853ac293683c33d183af9c9a0e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-23 18:47:34 -0500

    Update ProphecyToMockeryRector.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Rule/ProphecyToMockeryRector.php
+++ src/Rule/ProphecyToMockeryRector.php
@@ -11,7 +11,7 @@ use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 
 /**
- * @see \Ghostwriter\MockeryRectorTests\Unit\Rule\ProphecyToMockeryRectorTest
+ * @see \Tests\Unit\Rule\ProphecyToMockeryRectorTest
  */
 final class ProphecyToMockeryRector extends AbstractMockeryRector
 {
