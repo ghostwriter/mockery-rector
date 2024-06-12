26153361f2b7ca4ac1f052d91fc8e70b9da3efab
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-12 10:53:19 -0500

    Update UseMockeryPHPUnitIntegrationTraitRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Rule/UseMockeryPHPUnitIntegrationTraitRectorTest.php
+++ tests/Unit/Rule/UseMockeryPHPUnitIntegrationTraitRectorTest.php
@@ -6,9 +6,11 @@ namespace Tests\Unit\Rule;
 
 use Ghostwriter\MockeryRector\AbstractMockeryRector;
 use Ghostwriter\MockeryRector\Rule\UseMockeryPHPUnitIntegrationTraitRector;
-use Tests\Unit\AbstractMockeryRectorTestCase;
 use PHPUnit\Framework\Attributes\CoversClass;
+use Tests\Unit\AbstractMockeryRectorTestCase;
 
 #[CoversClass(UseMockeryPHPUnitIntegrationTraitRector::class)]
 #[CoversClass(AbstractMockeryRector::class)]
-final class UseMockeryPHPUnitIntegrationTraitRectorTest extends AbstractMockeryRectorTestCase {}
+final class UseMockeryPHPUnitIntegrationTraitRectorTest extends AbstractMockeryRectorTestCase
+{
+}
