89070951b88b4ad7d90e5d0abace222c6ad41c02
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-05 05:47:17 -0500

    Update MockerySetList.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/MockerySetList.php
+++ src/MockerySetList.php
@@ -4,15 +4,31 @@ declare(strict_types=1);
 
 namespace Ghostwriter\MockeryRector;
 
-use Rector\Set\Contract\SetListInterface;
+use Override;
+use Rector\Set\Contract\SetProviderInterface;
+use Rector\Set\ValueObject\Set;
 
-final readonly class MockerySetList implements SetListInterface
+final readonly class MockerySetList implements SetProviderInterface
 {
-    final public const MOCKERY_1_6 = __DIR__ . '/../config/mockery-1.6.php';
+    public const MOCKERY_1_6 = __DIR__ . '/../config/mockery-1.6.php';
 
-    final public const MOCKERY_2_0 = __DIR__ . '/../config/mockery-2.0.php';
+    public const MOCKERY_2_0 = __DIR__ . '/../config/mockery-2.0.php';
 
-    final public const PHPUNIT_TO_MOCKERY = __DIR__ . '/../config/phpunit-to-mockery.php';
+    public const PHPUNIT_TO_MOCKERY = __DIR__ . '/../config/phpunit-to-mockery.php';
 
-    final public const PROPHECY_TO_MOCKERY = __DIR__ . '/../config/prophecy-to-mockery.php';
+    public const PROPHECY_TO_MOCKERY = __DIR__ . '/../config/prophecy-to-mockery.php';
+
+    /**
+     * @return list<Set>
+     */
+    #[Override]
+    public function provide(): array
+    {
+        return [
+            new Set('Mockery', 'Mockery 1.6', self::MOCKERY_1_6),
+            new Set('Mockery', 'Mockery 2.0', self::MOCKERY_2_0),
+            new Set('Mockery', 'PHPUnit to Mockery', self::PHPUNIT_TO_MOCKERY),
+            new Set('Mockery', 'Prophecy to Mockery', self::PROPHECY_TO_MOCKERY),
+        ];
+    }
 }
