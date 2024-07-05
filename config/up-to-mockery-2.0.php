0cdeb61543708c016b3ec883699f0ccc8b84748a
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-05 09:34:31 -0500

    Update up-to-mockery-2.0.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- config/up-to-mockery-2.0.php
+++ config/up-to-mockery-2.0.php
@@ -7,4 +7,4 @@ use Ghostwriter\MockeryRector\MockerySetList;
 use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
-    ->withSets([MockerySetList::MOCKERY_2_0, MockeryLevelSetList::UP_TO_MOCKERY_1_6]);
+    ->withSets([MockerySetList::MOCKERY_2_0, MockeryLevelSetList::UPGRADE_TO_MOCKERY_1_6]);
