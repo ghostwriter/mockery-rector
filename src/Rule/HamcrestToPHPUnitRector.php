e966ec9fa171c250dafd740b9f423848eff22528
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-05 09:34:31 -0500

    Update HamcrestToPHPUnitRector.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Rule/HamcrestToPHPUnitRector.php
+++ src/Rule/HamcrestToPHPUnitRector.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Ghostwriter\MockeryRector\Rule;
 
 use Ghostwriter\MockeryRector\AbstractMockeryRector;
+use Override;
 use PhpParser\Node;
 use PhpParser\Node\Stmt\Class_;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
@@ -15,24 +16,29 @@ use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
  */
 final class HamcrestToPHPUnitRector extends AbstractMockeryRector
 {
+    #[Override]
     public function getRuleDefinition(): RuleDefinition
     {
-        return new RuleDefinition('Refactor Hamcrest matchers to PHPUnit constraints', [
-            new CodeSample(
-                <<<'CODE_SAMPLE'
-                    // @todo fill code before
-                    CODE_SAMPLE
-                ,
-                <<<'CODE_SAMPLE'
-                    // @todo fill code after
-                    CODE_SAMPLE
-            ),
-        ]);
+        return new RuleDefinition(
+            description: 'Refactor Hamcrest matchers to PHPUnit constraints',
+            codeSamples: [
+                new CodeSample(
+                    badCode: <<<'CODE_SAMPLE'
+                        // @todo fill code before
+                        CODE_SAMPLE
+                    ,
+                    goodCode: <<<'CODE_SAMPLE'
+                        // @todo fill code after
+                        CODE_SAMPLE
+                ),
+            ]
+        );
     }
 
     /**
      * @param Class_ $node
      */
+    #[Override]
     public function refactor(Node $node): ?Node
     {
         return $node;
