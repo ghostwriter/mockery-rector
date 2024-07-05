045088f4f64eecbfd169fc08580a341b5e24ffbb
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-05 05:47:17 -0500

    Update MockeryLevelSetList.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/MockeryLevelSetList.php
+++ src/MockeryLevelSetList.php
@@ -4,11 +4,25 @@ declare(strict_types=1);
 
 namespace Ghostwriter\MockeryRector;
 
-use Rector\Set\Contract\SetListInterface;
+use Override;
+use Rector\Set\Contract\SetProviderInterface;
+use Rector\Set\ValueObject\Set;
 
-final readonly class MockeryLevelSetList implements SetListInterface
+final readonly class MockeryLevelSetList implements SetProviderInterface
 {
-    final public const UP_TO_MOCKERY_1_6 = __DIR__ . '/../config/up-to-mockery-1.6.php';
+    public const UPGRADE_TO_MOCKERY_1_6 = __DIR__ . '/../config/up-to-mockery-1.6.php';
 
-    final public const UP_TO_MOCKERY_2_0 = __DIR__ . '/../config/up-to-mockery-2.0.php';
+    public const UPGRADE_TO_MOCKERY_2_0 = __DIR__ . '/../config/up-to-mockery-2.0.php';
+
+    /**
+     * @return list<Set>
+     */
+    #[Override]
+    public function provide(): array
+    {
+        return [
+            new Set('Mockery', 'Upgrade to v1.6', self::UPGRADE_TO_MOCKERY_1_6),
+            new Set('Mockery', 'Upgrade to v2.0', self::UPGRADE_TO_MOCKERY_2_0),
+        ];
+    }
 }
